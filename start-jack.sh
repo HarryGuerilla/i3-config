85fca82062414e100eedcb19b9abd49f94c6a2b9
Tony Pelaez <tnyplz@gmail.com>
2016-02-19 21:48:26 -0800

    start jack on startup

--- start-jack.sh
+++ start-jack.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 jack_control start
-sudo schedtool -R -p 20 `pidof jackdbus`
+#sudo schedtool -R -p 20 `pidof jackdbus`
 #jack_control eps realtime true
 jack_control ds alsa
 jack_control dps device hw:CODEC
 jack_control dps rate 48000
 jack_control dps nperiods 3
-jack_control dps period 64
-sleep 10
-/usr/bin/a2jmidid -e &
-sleep 10
+jack_control dps period 512
+# sleep 10
+# /usr/bin/a2jmidid -e &
+# sleep 10
 # sudo schedtool -R -p 20 `pidof a2jmidid`
-qjackctl &
-sleep 10
+# qjackctl &
+# sleep 10
 # sudo schedtool -R -p 20 `pidof qjackctl`
-qmidiroute /home/username/All2MIDI1.qmr &
-sleep 10
+# qmidiroute /home/username/All2MIDI1.qmr &
+# sleep 10
 # sudo schedtool -R -p 20 `pidof qmidiroute`
-yoshimi -S &
-sleep 10
+# yoshimi -S &
+# sleep 10
 # sudo schedtool -R -p 20 `pidof yoshimi`
